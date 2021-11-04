bc17d198290fc328f1dc4f1c38e32d7e3b1be313
John Bryan <johnbryan@Johns-iMac.lan>
2021-11-03 22:50:10 -0400

    updated app.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -7,8 +7,9 @@ class App extends Component {
   constructor() {
     super ();
     this.state = [{
-      location: [{ lat: 46.9171876, lng: 17.8951832 }, 'unknown'],
+      location: [{ lat: 46.9171876, lng: 17.8951832 }, 'hungary'],
       country: location[1],
+      score: 1,
       streetView: {
         position: location[0],
         pov: {
@@ -22,30 +23,57 @@ class App extends Component {
       }
     }];
     this.resetMap = this.resetMap.bind(this);
+    this.incrementScore = this.incrementScore.bind(this);
     this.handleButtonClick = this.handleButtonClick.bind(this);
   }
 
   // componentDidMount() {
-  //   this.resetMap();
+  //   fetch ('http://localhost:3000/coordinates')
+  //     .then(res => res.json())
+  //     .then(data => data[Math.floor(Math.random()*data.length)])
+  //     .then(data => {
+  //       this.setState({
+  //         location: data,
+  //         country: data[1],
+  //         score: this.score,
+  //         streetView: {
+  //           position: data[0],
+  //           pov: {
+  //           heading: 100,
+  //           pitch: 0
+  //           },
+  //           zoom: 1,
+  //           showRoadLabels: false,
+  //           addressControl: false,
+  //           linksControl: false,
+  //         }
+  //       })
+  //     })
   // }
 
   resetMap() {
     console.log('map reset');
     const locations = [
-      [{ lat: 40.723102, lng: -73.944107 }, 'brooklyn'],
-      [{ lat: 54.787284, lng: 32.063848 }, 'russia'],
+      [{ lat: 46.9171876, lng: 17.8951832 }, 'hungary'],
+      [{ lat: 55.749445, lng: 37.624346 }, 'russia'],
       [{ lat: 15.663209, lng: -96.512044 }, 'mexico'],
-      [{ lat: 42.155043, lng: 76.734303 }, 'kyrgyzstan'],
-      [{ lat: 9.148677, lng: -75.776268 }, 'peru'],
-      [{ lat: 2.677230, lng: 37.247880 }, 'kenya'], 
+      [{ lat: 42.131053, lng: 77.007336 }, 'kyrgyzstan'],
+      [{ lat: 48.851774, lng: 2.349619 }, 'france'],
+      [{ lat: 29.979069, lng: 31.132831 }, 'egypt'], 
+      [{ lat: 40.71484, lng: -73.99805 }, 'usa'], 
+      [{ lat: 47.804216, lng:  107.528345}, 'mongolia'],
+      [{ lat: 35.391850, lng:  138.606416}, 'japan'],
+      [{ lat: -1.377668, lng:  34.893306}, 'kenya'],
     ];
     let newLocation = locations[Math.floor(Math.random()*(locations.length))];
     console.log(newLocation[1]);
+    console.log(this.state.score);
     const newPosition = newLocation[0];
     const newCountry = newLocation[1];
     this.setState({
       location: newLocation,
       country: newCountry,
+      score: this.score,
       streetView: {
         position: newPosition,
         pov: {
@@ -60,16 +88,28 @@ class App extends Component {
     })
   }
 
+  incrementScore() {
+    this.setState((state) => {
+      return {
+        score: state.score + 1
+      }
+    });
+  }
+
   handleButtonClick() {
-    console.log('button clicked')
-    const input = window.prompt('take a guess');
+    const input = window.prompt('where are you?');
     console.log('guess ' + input);
     console.log('country: ' + this.state.country);
     if (input.toLowerCase() === this.state.country) {
-      alert('you are the geodude');
+      // if (this.state.score === 10) {
+      //   alert('you are the GEO-DUDE');
+      //   this.resetMap();
+      // }
+      alert('CORRECTO. PUNTO PARA TI');
+      this.incrementScore();
       this.resetMap();
     } else {
-      alert('guess again my friend');
+      alert('Ayayay try again');
       this.resetMap();
     }
   }
@@ -78,7 +118,8 @@ class App extends Component {
     const googleMapsApiKey = 'AIzaSyAG8pD29eYb7EnZNrNFinFbmMtJiqqnzKI';
     return (
       <div className='app' style={{width: '100%', height: '100%', backgroundColor: '#eeeeee'}}>
-        <button className='button' onClick={this.handleButtonClick}>Click me</button>
+        <button className='guess' onClick={this.handleButtonClick}>I'm feeling lucky</button>
+        <button className='scorecard'>High Scores</button>
         <ReactStreetview
           apiKey={googleMapsApiKey}
           streetViewPanoramaOptions={this.state.streetView}
